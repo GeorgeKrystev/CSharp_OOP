47d9b9da59cfe601a9d3083af3b892eda86491ae
GeorgeKrystev <george.krystew@gmail.com>
2016-04-08 17:50:33 +0300

    Adding exercie folder

---
+++ StaticMethods_And_Namespaces/BitArray/BitArray32.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class BitArray32
+{
+    private int number;
+
+    public BitArray32(int number)
+    {
+        this.number = number;
+    }
+
+    public void SetBitAt(int bit, int index)
+    {
+        if(index < 0 || index > 31)
+        {
+
+            throw new ArgumentOutOfRangeException("index", "Index must be in range [0; 31]");
+        }
+
+        if (bit != 1 && bit != 0)
+        {
+            throw new ArgumentException("index", "Bit should be either 0 or 1");
+
+        }
+
+        int nullified = this.number & ~(1 << index);
+
+        nullified |= bit << index;
+
+        this.number = nullified;
+    }
+
+    public int GetBit(int index)
+    {
+        if(index < 0 || index > 31)
+        {
+            throw new ArgumentOutOfRangeException("index", "Index must be in range [0; 31]");
+        }
+        int bit = (this.number >> index) & 1;
+        
+        return bit;
+    }
+} 
