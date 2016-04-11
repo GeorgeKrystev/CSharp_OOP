4bfa980cdaaebbac143b3e735ec9fc9f78db19ca
GeorgeKrystev <george.krystew@gmail.com>
2016-04-11 14:54:29 +0300

    Problem 3 solved

---
+++ StaticMethods_And_Namespaces_Homework/StaticMethods_And_Namespaces/_3Path3D/Path3D.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;  
+using Point;
+
+namespace _3Path3D
+{
+    class Path3D
+    {
+        private List<Point3D> path = new List<Point3D>();
+
+        public Path3D(List<Point3D> path = null) 
+        {
+            this.path = path;
+        }
+
+        public List<Point3D> Path
+        {
+            get { return this.path; }
+            set
+            {
+                this.path = value ?? new List<Point3D>();
+            }
+        }
+
+        public void AddPointToPath(Point3D point)
+        {
+            var currentPath = this.Path;
+            currentPath.Add(point);
+            this.Path = currentPath;
+        }
+
+        public override string ToString()
+        {
+            string result = "Path: \r\n";
+
+            foreach (var poin in this.Path)
+            {
+                result += "\t" + poin.ToString() + "\r\n";
+            }
+            return result;
+        }
+    }
+
+}
